5e9eba09204cbbaf0e9e880292ea573d2c486fde
Dylan Player <dylanplayer@Dylans-MacBook-Pro-2.local>
2022-08-18 14:59:04 -0700

    Variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -7,14 +7,30 @@ fn main() {
     // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
     // println!("Three hours in seconds {THREE_HOURS_IN_SECONDS}");
 
-    let x = 5;
+    // let x = 5;
 
-    let x = x + 1;
+    // let x = x + 1;
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in the inner scope is: {x}");
+    // }
 
-    println!("The value of x in the outer scope is: {x}");
+    // println!("The value of x in the outer scope is: {x}");
+
+    // let x: u8 = 255;
+    // let x: i8 = -127;
+    // let x: u16 = 65535;
+    // let x: i16 = -32767;
+    // let x: f64 = 123456789.123456789;
+    // println!("x: {x}");
+
+    let x = 56.7;
+    let y = 32.2;
+    
+    let sum = x + y;
+    println!("Sum: {sum}");
+
+    let difference = x - y;
+    println!("Difference: {difference}");
 }
